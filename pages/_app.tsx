9447ed14b0647213df88e9435c5e5c96b53d9418
fysakici <yagizsakicidev@gmail.com>
2025-07-28 22:22:11 +0300

    feat(auth): configure NextAuth with GitHub provider and Prisma adapter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
-import { AppProps } from "next/app";
+import { SessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
+import Header from '../components/Header';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 };
 
 export default App;
+
